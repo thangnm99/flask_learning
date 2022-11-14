114347a42e6b1faf79172558729dfb3f549f4bb1
Thang.nguyen <thang.nguyen@bnksolution.com>
2022-11-14 17:35:02 +0700

    update code to chapter 4

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     # mockup user
     user = {'username': 'Civix'}
@@ -25,6 +29,17 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'Login request for user {form.username.data}, rememeber me={form.remember_me.data}')
-        return redirect(url_for(index))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
--- microblog/config.py
+++ microblog/config.py
@@ -1,4 +1,11 @@
 import os
 
+BASEDIR = os.path.join(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    #Flask SQLAlchemy use this name for path of db
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATION = False
+
