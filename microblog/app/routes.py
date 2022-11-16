a4947b54ecb69ceac0b1c4eaf74dca5b0a8de90e
Thang.nguyen <thang.nguyen@bnksolution.com>
2022-11-16 15:34:54 +0700

    update to chapter 5

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
@@ -11,7 +11,7 @@ from werkzeug.urls import url_parse
 @login_required
 def index():
     # mockup user
-    user = {'username': 'Civix'}
+    # user = {'username': 'Civix'}
     posts = [
         {
             'author': {'username': 'Jonh'},
@@ -22,7 +22,7 @@ def index():
             'body': 'Gintama the movie 2 is so cool :D'
         },
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login',  methods=['GET', 'POST'])
@@ -32,6 +32,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('invalid username or password')
+            return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
@@ -43,3 +44,19 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Done, You are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
